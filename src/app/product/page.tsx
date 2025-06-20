b3534de67662ce4e2b76d0912b1e0bb6f315690e
Keegan Baobabb <keeganbaobabb@gmail.com>
2025-06-20 09:47:48 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="mb-8">

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -12,17 +12,6 @@ export default function ProductPage() {
   return (
     <div className="flex flex-col min-h-screen bg-white text-black">
       <main className="container mx-auto px-4 py-8 flex-grow">
-        <FadeInScroll threshold={0.01}>
-          <div className="mb-8">
-            <Button variant="outline" asChild className="border-gray-400 text-black hover:bg-buttonCta-text hover:text-white hover:border-buttonCta-text">
-              <Link href="/">
-                <ArrowLeft className="mr-2 h-4 w-4" />
-                Back to Home
-              </Link>
-            </Button>
-          </div>
-        </FadeInScroll>
-
         <FadeInScroll threshold={0.1} delay="md:delay-100">
           <ProductHero />
         </FadeInScroll>
@@ -36,7 +25,19 @@ export default function ProductPage() {
         </FadeInScroll>
 
       </main>
+
       <FadeInScroll threshold={0.1} delay="md:delay-400">
+        <div className="container mx-auto px-4 flex justify-center py-8">
+          <Button variant="outline" asChild className="border-gray-400 text-black hover:bg-buttonCta-text hover:text-white hover:border-buttonCta-text">
+            <Link href="/">
+              <ArrowLeft className="mr-2 h-4 w-4" />
+              Back to Home
+            </Link>
+          </Button>
+        </div>
+      </FadeInScroll>
+
+      <FadeInScroll threshold={0.1} delay="md:delay-500">
         <Footer />
       </FadeInScroll>
     </div>
