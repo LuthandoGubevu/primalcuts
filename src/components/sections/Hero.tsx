e6fb98176d5e5a1450e39a50d3c25d5a4bfe498d
Keegan Baobabb <keeganbaobabb@gmail.com>
2025-06-21 16:13:13 +0000

    this is the space I want between the two elements

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -64,7 +64,7 @@ export default function Hero({
       </div>
 
       {/* Decorative Elements at Bottom */}
-      <div className="absolute bottom-0 left-0 w-full z-0 flex flex-col items-center gap-0.5 pointer-events-none">
+      <div className="absolute bottom-0 left-0 w-full z-0 flex flex-col items-center pointer-events-none">
         <Image
           src="/PC Elements-21.png"
           alt="Decorative border element"
