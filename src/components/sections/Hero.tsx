80fa9076f0862d4ee4ad3b3bda1d4baf1cf468d0
Keegan Baobabb <keeganbaobabb@gmail.com>
2025-06-23 09:06:51 +0000

    ensure that the "PC Elements-02-New-01.png" has no spaces on the left, r

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Hero() {
   return (
-    <section className="relative h-[400px] md:h-[500px] lg:h-[600px] w-full flex items-center justify-center">
+    <section className="relative h-[400px] md:h-[500px] lg:h-[600px] w-full">
       {/* Background Image */}
       <Image
         src="/crocodile.jpg"
@@ -13,14 +13,14 @@ export default function Hero() {
         priority
       />
       
-      {/* Foreground Image container */}
-      <div className="relative z-10 container mx-auto px-4 py-8 flex justify-center items-center">
+      {/* Foreground Image container, absolutely positioned at the bottom */}
+      <div className="absolute bottom-0 left-0 right-0 z-10">
         <Image
           src="/PC Elements-02-New-01.png"
           alt="Primal Cuts Banner"
           width={1200}
           height={400}
-          className="w-full h-auto max-w-4xl object-contain"
+          className="w-full h-auto object-contain"
           data-ai-hint="decorative banner"
         />
       </div>
