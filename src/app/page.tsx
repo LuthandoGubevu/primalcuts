775ae048e18271f66fc309c419ca3d6dabb47bcf
Keegan Baobabb <keeganbaobabb@gmail.com>
2025-06-23 06:22:11 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="flex f

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,16 +68,6 @@ export default function HomePage() {
       </FadeInScroll>
 
       <Highlight
-        imageSrc="/PC-Elements-11.png"
-        imageAlt="Three packages of Primal Cuts biltong"
-        imageAiHint="product packages lifestyle"
-        features={healthFeatures}
-        imagePosition="left"
-        variant="bleed"
-        className="bg-secondary/20"
-      />
-
-      <Highlight
         imageSrc="/black-pepper.jpg"
         imageAlt="Man seasoning strips of beef in a commercial kitchen"
         imageAiHint="chef seasoning meat"
@@ -87,6 +77,16 @@ export default function HomePage() {
         variant="bleed"
         displayMode="paragraph"
         className="bg-[#07080c] text-primary-foreground"
+      />
+
+      <Highlight
+        imageSrc="/PC-Elements-11.png"
+        imageAlt="Three packages of Primal Cuts biltong"
+        imageAiHint="product packages lifestyle"
+        features={healthFeatures}
+        imagePosition="left"
+        variant="bleed"
+        className="bg-[#f0f0f0]"
       />
       
       <FadeInScroll threshold={0.1} delay="md:delay-100">
