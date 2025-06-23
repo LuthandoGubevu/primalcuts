bc2ea61e07ed1e92430b8d913129d9ca9a284fec
Keegan Baobabb <keeganbaobabb@gmail.com>
2025-06-23 12:34:58 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/product/ProductImage.tsx
+++ src/components/product/ProductImage.tsx
@@ -4,16 +4,8 @@ import Image from 'next/image';
 export default function ProductImage() {
   return (
     <>
-      <svg className="w-0 h-0 absolute">
-        <defs>
-          <clipPath id="hero-bottom-clip" clipPathUnits="objectBoundingBox">
-            <path d="M0,0.8 C0.16,0.95 0.33,0.95 0.5,0.8 C0.66,0.65 0.83,0.65 1,0.8 V0 H0 Z" />
-          </clipPath>
-        </defs>
-      </svg>
       <section
         className="relative h-[1080px] w-full"
-        style={{ clipPath: 'url(#hero-bottom-clip)' }}
       >
         {/* Background Image */}
         <Image
